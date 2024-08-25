f96066b0cfbee7fbb3d27c394a6c23399c68d235
stepanoid <123142019+stepanoid@users.noreply.github.com>
2024-08-25 16:07:30 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TELEGRAM_TOKEN || 'TELEGRAM_BOT_TOKEN';
+const bot = new TelegramBot(token, { polling: true });
+
+// /start
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'Hi! I am your Bot!');
+});
+
+console.log('the bot is working...');
