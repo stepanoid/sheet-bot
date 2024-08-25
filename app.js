fee107e07ec86a4b4c1a0dde4d3c45de71cab9cd
stepanoid <123142019+stepanoid@users.noreply.github.com>
2024-08-25 16:23:48 +0300

    add /secret message

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ bot.onText(/\/help/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Available commands: \n/start - Greeting, \n/help - List of commands');
 });
 
+// /secret
+bot.onText(/\/secret/, (msg) => {
+    bot.sendMessage(msg.chat.id, 'The most “secret” secret of programming might be that no one knows everything. Even the most experienced programmers encounter bugs, get stuck, and have to Google solutions. Programming is a continuous learning process, and the real secret is persistence, curiosity, and the willingness to keep learning and improving.');
+});
+
 console.log('the bot is working...');
